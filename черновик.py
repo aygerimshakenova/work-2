1a5346df6383356c2eaa2ac8d63ef0c784d9f6b7
aigerimshakenova <laventawind@gmail.com>
2022-06-14 12:06:16 +0600

    черновик

---
+++ черновик.py
@@ -0,0 +1,40 @@
+# num1 = int(input("Введите первое число: "))
+# num2 = int(input("Введите второе число: "))
+
+# num = "husd#hgieolf#he;ye#wih"
+# print(num.split('#'))
+# print(num.replace("e", "*"))
+
+# num1 = int(input("Введите первое число: "))
+# num2 = int(input("Введите второе число: "))
+# num3 = input("Введите действие: ")
+
+# if num3 == "+":
+#     print(f"Ответ: {num1 + num2}")
+# elif num3 == "-":
+#     print(f"Ответ: {num1 - num2}")
+# elif num3 == "*":
+#     print(f"Ответ: {num1 * num2}")
+# elif num3 == "/":
+#     print(f"Ответ: {num1 / num2}")
+   
+
+# a = 1
+# b = 2
+# c = 3
+# a,b,c = c,a,b
+# print(a,b,c)
+
+# stroka = "Love"
+# print(stroka[-1])
+# print(stroka[-2:])
+# print(stroka[::-1])
+# print(len(stroka))
+
+
+
+
+a = {'apple': 0.40, 'orange': 0.35, 'banana': 0.25}
+for val in a.values():
+    if (val > 0) / 5:
+        print(a)
